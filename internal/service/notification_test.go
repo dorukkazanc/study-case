ed01c94f663160fe7fe14fe9342386ffc052155b
dorukkazanc <106471211+dorukkazanc@users.noreply.github.com>
2026-05-16 09:52:24 +0300

    add dummy logger to service tests

--- internal/service/notification_test.go
+++ internal/service/notification_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -13,7 +14,7 @@ import (
 )
 
 func newService(repo *mockRepository, queue *mockQueue) service.Service {
-	return service.NewNotificationService(repo, queue)
+	return service.NewNotificationService(repo, queue, zerolog.Nop())
 }
 
 func TestCreate_Success(t *testing.T) {
